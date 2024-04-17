a8d4d7b419df8e8c4be28adc9ebc6c33edaa6a01
gerard-figma <gerard-figma@users.noreply.github.com>
2024-04-17 18:42:54 +0000

    Update tokens from Figma

---
+++ build/ios/enum.swift
@@ -0,0 +1,76 @@
+
+//
+// enum.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 17 Apr 2024 18:42:53 GMT
+
+
+import UIKit
+
+public enum  {
+    public static let buttonHeight = 60
+    public static let buttonWidth = 117.5
+    public static let colorBlack100 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorBlack200 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let colorGray100 = UIColor(red: 0.961, green: 0.961, blue: 0.961, alpha: 1)
+    public static let colorGray1000 = UIColor(red: 0.125, green: 0.125, blue: 0.125, alpha: 1)
+    public static let colorGray200 = UIColor(red: 0.898, green: 0.898, blue: 0.898, alpha: 1)
+    public static let colorGray300 = UIColor(red: 0.835, green: 0.835, blue: 0.835, alpha: 1)
+    public static let colorGray400 = UIColor(red: 0.702, green: 0.702, blue: 0.702, alpha: 1)
+    public static let colorGray500 = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
+    public static let colorGray600 = UIColor(red: 0.494, green: 0.494, blue: 0.494, alpha: 1)
+    public static let colorGray700 = UIColor(red: 0.341, green: 0.341, blue: 0.341, alpha: 1)
+    public static let colorGray800 = UIColor(red: 0.220, green: 0.220, blue: 0.220, alpha: 1)
+    public static let colorGray900 = UIColor(red: 0.153, green: 0.153, blue: 0.153, alpha: 1)
+    public static let colorGreen100 = UIColor(red: 0.851, green: 0.918, blue: 0.902, alpha: 1)
+    public static let colorGreen1000 = UIColor(red: 0.000, green: 0.094, blue: 0.071, alpha: 1)
+    public static let colorGreen200 = UIColor(red: 0.702, green: 0.835, blue: 0.804, alpha: 1)
+    public static let colorGreen300 = UIColor(red: 0.525, green: 0.710, blue: 0.663, alpha: 1)
+    public static let colorGreen400 = UIColor(red: 0.490, green: 0.722, blue: 0.663, alpha: 1)
+    public static let colorGreen500 = UIColor(red: 0.247, green: 0.561, blue: 0.482, alpha: 1)
+    public static let colorGreen600 = UIColor(red: 0.086, green: 0.498, blue: 0.396, alpha: 1)
+    public static let colorGreen700 = UIColor(red: 0.000, green: 0.455, blue: 0.341, alpha: 1)
+    public static let colorGreen800 = UIColor(red: 0.000, green: 0.337, blue: 0.255, alpha: 1)
+    public static let colorGreen900 = UIColor(red: 0.000, green: 0.216, blue: 0.161, alpha: 1)
+    public static let colorPurple100 = UIColor(red: 0.882, green: 0.737, blue: 1.000, alpha: 1)
+    public static let colorPurple1000 = UIColor(red: 0.157, green: 0.055, blue: 0.235, alpha: 1)
+    public static let colorPurple200 = UIColor(red: 0.863, green: 0.780, blue: 0.925, alpha: 1)
+    public static let colorPurple300 = UIColor(red: 0.804, green: 0.702, blue: 0.980, alpha: 1)
+    public static let colorPurple400 = UIColor(red: 0.800, green: 0.541, blue: 1.000, alpha: 1)
+    public static let colorPurple500 = UIColor(red: 0.698, green: 0.376, blue: 0.941, alpha: 1)
+    public static let colorPurple600 = UIColor(red: 0.576, green: 0.333, blue: 0.761, alpha: 1)
+    public static let colorPurple700 = UIColor(red: 0.541, green: 0.271, blue: 0.749, alpha: 1)
+    public static let colorPurple800 = UIColor(red: 0.365, green: 0.122, blue: 0.549, alpha: 1)
+    public static let colorPurple900 = UIColor(red: 0.231, green: 0.078, blue: 0.349, alpha: 1)
+    public static let colorRed100 = UIColor(red: 0.957, green: 0.863, blue: 0.875, alpha: 1)
+    public static let colorRed1000 = UIColor(red: 0.392, green: 0.047, blue: 0.090, alpha: 1)
+    public static let colorRed200 = UIColor(red: 0.914, green: 0.725, blue: 0.749, alpha: 1)
+    public static let colorRed300 = UIColor(red: 1.000, green: 0.620, blue: 0.671, alpha: 1)
+    public static let colorRed400 = UIColor(red: 0.965, green: 0.525, blue: 0.584, alpha: 1)
+    public static let colorRed500 = UIColor(red: 0.918, green: 0.408, blue: 0.475, alpha: 1)
+    public static let colorRed600 = UIColor(red: 0.878, green: 0.314, blue: 0.384, alpha: 1)
+    public static let colorRed700 = UIColor(red: 0.592, green: 0.847, blue: 0.055, alpha: 1)
+    public static let colorRed800 = UIColor(red: 0.714, green: 0.086, blue: 0.165, alpha: 1)
+    public static let colorRed900 = UIColor(red: 0.573, green: 0.071, blue: 0.133, alpha: 1)
+    public static let colorWhite100 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorYellow100 = UIColor(red: 1.000, green: 0.969, blue: 0.851, alpha: 1)
+    public static let colorYellow1000 = UIColor(red: 0.278, green: 0.220, blue: 0.000, alpha: 1)
+    public static let colorYellow200 = UIColor(red: 1.000, green: 0.933, blue: 0.702, alpha: 1)
+    public static let colorYellow300 = UIColor(red: 0.925, green: 0.839, blue: 0.525, alpha: 1)
+    public static let colorYellow400 = UIColor(red: 0.945, green: 0.827, blue: 0.404, alpha: 1)
+    public static let colorYellow500 = UIColor(red: 0.965, green: 0.812, blue: 0.263, alpha: 1)
+    public static let colorYellow600 = UIColor(red: 0.988, green: 0.792, blue: 0.086, alpha: 1)
+    public static let colorYellow700 = UIColor(red: 1.000, green: 0.780, blue: 0.000, alpha: 1)
+    public static let colorYellow800 = UIColor(red: 0.839, green: 0.655, blue: 0.000, alpha: 1)
+    public static let colorYellow900 = UIColor(red: 0.639, green: 0.498, blue: 0.000, alpha: 1)
+    public static let cornerRadius = 30
+    public static let frameWidthMax = 0
+    public static let frameWidthMin = 0
+    public static let showPrice = false
+    public static let showThumbnail = false
+    public static let spacingLg = 48
+    public static let spacingMd = 20
+    public static let spacingSm = 16
+}
